94c8fb2b6bdbe80c0a9e0091ff1b2d1bed4e1a99
André Oliveira <andrelitoscria@gmail.com>
2024-08-06 20:56:22 -0300

    mais uma aula concluída

---
+++ ex002/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Exemplo php</h1>
+    <?php 
+        $nome = "André Oliveira";
+        echo "<p>O meu nome é $nome </p>";
+    ?>
+</body>
+</html>

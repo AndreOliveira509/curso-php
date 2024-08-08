7cb5d4d53078abef79ff2203795b82f74af3aa22
André Oliveira <andrelitoscria@gmail.com>
2024-08-07 21:38:01 -0300

    .

---
+++ ex004/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $nome = "André";
+    $sobrenome = "Oliveira";
+    $nome = "Nycolas";
+    $sobrenome = "Silva";
+    const PAIS = "Brasil";
+    echo 'Oi meu nome é '. $nome . ', meu sobrenome é '. $sobrenome . 'e moro no '. PAIS;
+    ?>
+</body>
+</html>

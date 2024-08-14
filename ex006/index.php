b52d53911dbdb5f1a989a649384c20a996411274
André Oliveira <andrelitoscria@gmail.com>
2024-08-14 08:38:20 -0300

    .

---
+++ ex006/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    // const NOME = 'Recayd';
+        // $sobrenome = 'Mob';
+        // echo NOME ." $sobrenome pai do trap!";
+        $nome = 'Rodrigo';
+        $snome = 'Nogueira';
+        echo "$nome \"Minotauro\" $snome";
+    ?>
+</body>
+</html>
